2f8000f5fa4482f29ef32f6ffdb25b37a6de7b21
slava <osslav@mail.com>
2020-11-23 18:49:33 +0700

    create project

---
+++ class_list/class_list/List.h
@@ -0,0 +1,13 @@
+//#pragma once
+
+#ifndef list
+#define list
+
+#include "Node.h"
+
+class List
+{
+
+};
+
+#endif
---
+++ class_list/class_list/Node.h
@@ -0,0 +1,13 @@
+//#pragma once
+
+#ifndef node
+#define node
+
+
+
+class Node
+{
+
+};
+
+#endif
